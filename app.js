565f28e9cf93e838a53a6be049c45eeab1b07714
isaacross <Theisaacross@gmail.com>
2021-08-03 19:59:10 -0600

    Starting JS

---
+++ app.js
@@ -0,0 +1,9 @@
+class Tomagotchi{
+    constructor(name, hunger, tiredness, boredom){
+        this.name = name
+        this.hunger = 0
+        this.tiredness = 0
+        this.boredom = 0
+    }
+}
+const blitz = new Tomagotchi('Blitz')
