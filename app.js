4a80dcaaf0e96ac45c22978d51803c1a340f9bae
isaacross <Theisaacross@gmail.com>
2021-08-03 20:27:39 -0600

    usable button

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
+const hunger = document.querySelector('#hunger')
+const tiredness = document.querySelector('#tiredness')
+const boredom = document.querySelector('#boredom')
+
+const hungerButton = document.querySelector('#feed')
+const sleepButton = document.querySelector('#sleep')
+const playButton = document.querySelector('#play')
+
 class Tomagotchi{
     constructor(name, hunger, tiredness, boredom){
         this.name = name
-        this.hunger = 0
-        this.tiredness = 0
-        this.boredom = 0
+        this.hungerLevel = 0
+        this.tirednessLevel = 0
+        this.boredomLevel = 0
     }
 }
 const blitz = new Tomagotchi('Blitz')
+function feed(){
+    blitz.hungerLevel--
+    hunger.innerText = `Hunger: ${blitz.hungerLevel}/10`
+}
+function sleep(){
+    blitz.tirednessLevel--
+    tiredness.innerText = `Tiredness: ${blitz.tirednessLevel}/10`
+}
+function play(){
+    blitz.boredomLevel--
+    boredom.innerText = `Boredom: ${blitz.boredomLevel}/10`
+}
+const feedClick = hungerButton.addEventListener('click', feed)
+const sleepClick = sleepButton.addEventListener('click', sleep)
+const playClick = playButton.addEventListener('click', play)
+
